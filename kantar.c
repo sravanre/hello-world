05eb04369193067641517d31ae7f1b5419435e85
root <root@centos.c5pkawuohgiuppa021xcfoph0g.hx.internal.cloudapp.net>
2018-10-30 00:23:28 +0000

    second commit for kantar.c

--- kantar.c
+++ kantar.c
@@ -1,2 +1,9 @@
 for i in cat /etc/passwd
+
+adfadf
+
+dd if=/dev/zero of=/dev/null &
+dd if=/dev/zero of=/dev/null &
+dd if=/dev/zero of=/dev/null &
+dd if=/dev/zero of=/dev/null &
 dd if=/dev/zero of=/dev/null &
